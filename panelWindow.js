c6760f1f5a291d932dcbe9b05b899094bb4a71fb
Reygin <sreygin@gmail.com>
2026-05-08 00:13:35 +0800

    Added show hide text and panel opacity bg when click

--- panelWindow.js
+++ panelWindow.js
@@ -1,13 +1,26 @@
 const panelElement = document.querySelectorAll(".panel");
 let prevPanel;
+let prevSpan;
 
 panelElement.forEach((panel) => {
   panel.addEventListener("click", (event) => {
     if (prevPanel) {
       prevPanel.classList.remove("open-panel");
+      prevPanel.style.setProperty("--bg-color", "rgba(0, 0, 0, 0");
+      prevSpan.forEach((span) => {
+        span.style.removeProperty("transform");
+      });
     }
     const currentPannel = event.target.closest(".panel");
+    currentPannel.style.setProperty("--bg-color", "rgba(0, 0, 0, 0.5)");
     prevPanel = currentPannel;
     currentPannel.classList.add("open-panel");
+
+    const spanChildEl = panel.querySelectorAll("span:nth-child(odd)");
+    prevSpan = spanChildEl;
+
+    spanChildEl.forEach((span) => {
+      span.style.setProperty("transform", "initial");
+    });
   });
 });
